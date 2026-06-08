aab1038f017d137bc8905d3983f12d104540d1f3
AbdelrahmanY25 <abdoyasserhessan5@gmail.com>
2026-06-08 11:43:13 +0300

    Add validationon max pageSize number with max 50 item per page

--- src/Services/Catalog/Catalog.Api/Products/GetProducts/GetProductsEndPoint.cs
+++ src/Services/Catalog/Catalog.Api/Products/GetProducts/GetProductsEndPoint.cs
@@ -1,6 +1,7 @@
 namespace Catalog.Api.Products.GetProducts;
 
 public record GetProductsRequest(int? PageNumber = 1, int? PageSize = 10);
+
 public record GetProductsResponse(IEnumerable<Product> Products);
 
 public class GetProductsEndPoint : ICarterModule
--- src/Services/Catalog/Catalog.Api/Products/GetProducts/GetProductsQueryHandler.cs
+++ src/Services/Catalog/Catalog.Api/Products/GetProducts/GetProductsQueryHandler.cs
@@ -1,7 +1,23 @@
 namespace Catalog.Api.Products.GetProducts;
 
 public record GetProductsQuery(int? PageNumber = 1, int? PageSize = 10) : IQuery<GetProductsResult>;
+
 public record GetProductsResult(IEnumerable<Product> Products);
+
+public class GetProductRequestQueryValidator : AbstractValidator<GetProductsQuery>
+{
+	public GetProductRequestQueryValidator()
+	{
+		RuleFor(x => x.PageNumber)
+			.GreaterThan(0)
+			.WithMessage("Page number must be greater than 0.");
+
+		RuleFor(x => x.PageSize)
+			.GreaterThan(0)
+			.LessThanOrEqualTo(50)
+			.WithMessage("Page size must be less than or equal to 50.");
+	}
+}
 
 public class GetProductsQueryHandler(IDocumentSession session) : IQueryHandler<GetProductsQuery, GetProductsResult>
 {
