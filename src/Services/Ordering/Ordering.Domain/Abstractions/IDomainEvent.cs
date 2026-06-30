c5bf2861d46a2a79d626b8f767102bf3fa5f31eb
AbdelrahmanY25 <abdoyasserhessan5@gmail.com>
2026-06-30 16:02:34 +0300

    Add domain abstractions and integrate MediatR
    Introduced MediatR to Ordering.Domain and added a new abstractions layer with base classes and interfaces for entities, aggregates, and domain events. Implemented domain event handling and standardized domain modeling with IDomainEvent inheriting from INotification.

---
+++ src/Services/Ordering/Ordering.Domain/Abstractions/IDomainEvent.cs
@@ -0,0 +1,8 @@
+﻿namespace Ordering.Domain.Abstractions;
+
+public interface IDomainEvent : INotification
+{
+	Guid EventId => Guid.CreateVersion7();
+	public DateTime OccurredOn => DateTime.UtcNow;
+	public string EventType => GetType().AssemblyQualifiedName ?? GetType().FullName ?? GetType().Name;
+}
