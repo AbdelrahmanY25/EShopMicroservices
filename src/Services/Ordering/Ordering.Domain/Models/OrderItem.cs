82f9d922ffae09feb1bd68f02112997d24f3ec29
AbdelrahmanY25 <abdoyasserhessan5@gmail.com>
2026-06-30 17:54:40 +0300

    Deverlop Strongly Typed IDs for Order Entities

--- src/Services/Ordering/Ordering.Domain/Models/OrderItem.cs
+++ src/Services/Ordering/Ordering.Domain/Models/OrderItem.cs
@@ -1,8 +1,8 @@
 ﻿namespace Ordering.Domain.Models;
 
-public class OrderItem : Entity<Guid>
+public class OrderItem : Entity<OrderItemId>
 {
-	internal OrderItem(Guid orderId, Guid productId, int quantity, decimal price)
+	internal OrderItem(OrderId orderId, ProductId productId, int quantity, decimal price)
 	{
 		OrderId = orderId;
 		ProductId = productId;
@@ -10,8 +10,8 @@ public class OrderItem : Entity<Guid>
 		Price = price;
 	}
 
-	public Guid OrderId { get; private set; } = default!;
-	public Guid ProductId { get; private set; } = default!;
+	public OrderId OrderId { get; private set; } = default!;
+	public ProductId ProductId { get; private set; } = default!;
 	public int Quantity { get; private set; } = default!;
 	public decimal Price { get; private set; } = default!;
 }
