d7da3f35903ef6457339bad5a446558b53d32c1c
AbdelrahmanY25 <abdoyasserhessan5@gmail.com>
2026-07-06 16:06:50 +0300

    Add Pagination logic in BuildingBlocks Project

---
+++ src/BuildingBlocks/BuildingBlocks/Pagination/PaginatedResult.cs
@@ -0,0 +1,19 @@
+﻿namespace BuildingBlocks.Pagination;
+
+public class PaginatedResult<T>(List<T> items, int count, int pageNumber, int pageSize)
+{
+	public List<T> Items { get; } = items;
+	public int PageNumber { get; } = pageNumber;
+	public int TotalPages { get; } = (int)Math.Ceiling(count / (double)pageSize);
+	public bool HasPreviousPage => PageNumber > 1;
+	public bool HasNextPage => PageNumber < TotalPages;
+
+	public static async Task<PaginatedResult<T>> CreateAsync(IQueryable<T> source, int pageSize, int currentPageNumber)
+	{
+		int count = source.Count();
+
+		var items = source.Skip((currentPageNumber - 1) * pageSize).Take(pageSize).ToList();
+
+		return new(items, pageSize, currentPageNumber, count);
+	}
+}
---
+++ src/BuildingBlocks/BuildingBlocks/Pagination/Paginationrequest.cs
@@ -0,0 +1,16 @@
+﻿namespace BuildingBlocks.Pagination;
+
+public record PaginationRequest(int PageNumber = 1, int PageSize = 10);
+
+public class PaginationRequestValidator : AbstractValidator<PaginationRequest>
+{
+	public PaginationRequestValidator()
+	{
+		RuleFor(x => x.PageNumber)
+			.GreaterThan(0).WithMessage("Page number must be greater than 0.");
+
+		RuleFor(x => x.PageSize)
+			.GreaterThan(0).WithMessage("Page size must be greater than 0.")
+			.LessThanOrEqualTo(100).WithMessage("Page size must not exceed 100.");
+	}
+}	
