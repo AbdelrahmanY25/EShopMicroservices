5cf2041310ca86d1dc16bd1aea158569dd92bf08
AbdelrahmanY25 <abdoyasserhessan5@gmail.com>
2026-07-30 17:35:22 +0300

    Add BuildingBlocks.Messaging project and basket events.

---
+++ src/BuildingBlocks/BuildingBlocks.Messaging/Events/IntegrationEvent.cs
@@ -0,0 +1,8 @@
+﻿namespace BuildingBlocks.Messaging.Events;
+
+public record IntegrationEvent
+{
+	public Guid Id => Guid.CreateVersion7();
+	public DateTime OccuredOn => DateTime.UtcNow;
+	public string EventType => GetType().AssemblyQualifiedName;
+}
